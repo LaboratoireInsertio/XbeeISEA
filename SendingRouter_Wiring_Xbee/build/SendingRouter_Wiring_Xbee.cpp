057ad65ce214e3f7f9cbaa963d7ea1c6e1b5b8ed
Thomas O Fredericks <tof@t-o-f.info>
2016-05-04 11:24:06 -0400

    Initial file commit

---
+++ SendingRouter_Wiring_Xbee/build/SendingRouter_Wiring_Xbee.cpp
@@ -0,0 +1,115 @@
+/**
+ * Copyright (c) 2009 Andrew Rapp. All rights reserved.
+ *
+ * This file is part of XBee-Arduino.
+ *
+ * XBee-Arduino is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * XBee-Arduino is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with XBee-Arduino.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <XBee.h>
+
+/*
+This example is for Series 2 XBee
+ Sends a ZB TX request with the value of analogRead(pin5) and checks 
+ the status response for success
+ */
+
+// create the XBee object
+#include "WProgram.h"
+void flashLed(int pin, int times, int wait);
+void setup();
+void loop();
+void sendPacket(XBeeAddress64 addr64);
+XBee xbee = XBee();
+
+uint8_t payload[] = { 
+  0
+};
+
+// SH + SL Address of receiving XBee
+XBeeAddress64 addr64_n0 = XBeeAddress64(0x0013a200, 0x40e66c13);
+XBeeAddress64 addr64_n1 = XBeeAddress64(0x0013a200, 0x40e66c13);
+XBeeAddress64 addr64_n2 = XBeeAddress64(0x0013a200, 0x40e66c13);
+XBeeAddress64 addr64_n3 = XBeeAddress64(0x0013a200, 0x40e66c13);
+XBeeAddress64 addr64_n4 = XBeeAddress64(0x0013a200, 0x40e66c13);
+
+ZBTxRequest zbTx = ZBTxRequest(addr64_n0, payload, sizeof(payload));
+ZBTxStatusResponse txStatus = ZBTxStatusResponse();
+
+//int pin5 = 0;
+int val;
+
+int statusLed = 15;
+int errorLed = 15;
+
+void flashLed(int pin, int times, int wait) {
+
+  for (int i = 0; i < times; i++) {
+    digitalWrite(pin, HIGH);
+    delay(wait);
+    digitalWrite(pin, LOW);
+
+    if (i + 1 < times) {
+      delay(wait);
+    }
+  }
+}
+
+void setup() {
+  pinMode(statusLed, OUTPUT);
+  pinMode(errorLed, OUTPUT);
+
+  Serial.begin(115200);
+
+  Serial1.begin(57600);
+  xbee.setSerial(Serial1);
+}
+
+void loop() {
+  xbee.readPacket();
+  
+  // break down 10-bit reading into two bytes and place in payload
+ // pin5 = analogRead(5)/4;
+  if (Serial.available()){
+    val = int(Serial.read());
+   Serial.println(val, DEC);
+ }
+  //payload[0] = pin5 >> 8 & 0xff;
+  //payload[1] = pin5 & 0xff;
+  payload[0] = val;
+  
+  sendPacket(0x0013a20040e66c13);
+
+
+  
+
+  delay(50);
+}
+
+void sendPacket(XBeeAddress64 addr64) {
+  // Prepare the Zigbee Transmit Request API packet
+  ZBTxRequest txRequest;
+  txRequest.setAddress64(addr64);
+  uint8_t payload1[] = {
+    0
+  };
+  payload1[0] = val;
+  txRequest.setPayload(payload1, sizeof(payload1));
+  // And send it
+  xbee.send(txRequest);
+
+  
+}
+
+
